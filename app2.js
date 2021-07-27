0eac814cce0b959f4bef73ee328f2fc242b22dbf
Israel <israelhuallpara@gmail.com>
2021-07-26 22:10:34 -0400

    Concluido carrera de carritos

--- app2.js
+++ app2.js
@@ -1,23 +1,27 @@
+const meta = 1000
+
 function generaNumero() {
-    let numeroConDecimales = Math.random() * 10
+    let numeroConDecimales = Math.random() * 200
     console.log(Math.round(numeroConDecimales))
 }
-setInterval(() => {
+let intervalo = setInterval(() => {
     avanzarVehiculo("car1")
     avanzarVehiculo("car2")
     avanzarVehiculo("car3")
-
 }, 2000);
 
 function avanzarVehiculo(carro) {
 
-    let numeroConDecimales = Math.random() * 10
+    let numeroConDecimales = Math.random() * 100
     let cantidad = Math.round(numeroConDecimales)
     let marginleft = document.getElementById(carro).style.marginLeft.substr(0, document.getElementById(carro).style.marginLeft.length - 2)
     let aincrementar = Number(marginleft) + cantidad
     document.getElementById(carro).style.marginLeft = aincrementar + "px"
-    console.log(carro, (marginleft + cantidad))
-
+    console.log(carro, aincrementar, meta)
+    if(aincrementar>meta){
+        clearInterval(intervalo)
+        alert("Ganó el "+carro)
+    }
 }
 
 // Identificando el vehículo
