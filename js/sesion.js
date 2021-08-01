971d81b88ee0b0a7fa1528a017946b4b82476fc9
Israel <israelhuallpara@gmail.com>
2021-08-01 09:53:18 -0400

    Iniciando con php

--- js/sesion.js
+++
@@ -1,8 +0,0 @@
-function crearSesion(datos){
-    //guarda en localstorage los datos del usuario como token de sesion
-    localStorage.setItem('token', JSON.stringify(datos))
-}
-function cerrarSesion(){
-    //guarda en localstorage los datos del usuario como token de sesion
-    localStorage.removeItem('token')
-}
