0cdda65859d2c46d7329efa8370597d37092acd5
Israel <israelhuallpara@gmail.com>
2021-08-04 22:11:22 -0400

    GET para ATM

--- api/atm/index.php
+++ api/atm/index.php
@@ -1,10 +1,18 @@
 <?php
-    require("./utils.php");
+    //Controlador de atm
+    require_once("./../utils.php");
+    //Importando el modelo ATM
+    require("./Atm.php");
+    //Cambiando la cabecera de HTML a JSON
     header("Content-Type: application/json; charset=UTF-8");
+    $atmCamacho = new Atm();
+    $atmPerez = new Atm();
+    $atms = array($atmCamacho, $atmPerez);
+    
 
     switch($_SERVER["REQUEST_METHOD"]){
         case 'GET':
-            $respueta = array(mensaje=>"Solicitado por GET");
+            $respueta = $atmCamacho->imprimir();
             echo vectorAJson($respueta);
             break;
         case 'POST':
