d851512e01d0803c3b403a88957f499d84d3f2fb
Cesar Montes Blanco <cesarmontesb@gmail.com>
2021-08-06 20:47:11 -0400

    implementacion POST y PUT

--- api/atm/index.php
+++ api/atm/index.php
@@ -9,14 +9,15 @@
     //Obteniendo datos del body
     $parametros = file_get_contents('php://input');
     $_POST = json_decode($parametros, TRUE);
-
+    $_POST = json_decode($parametros, TRUE);
     $atmCamacho = new Atm(1,"Camacho");
     $atmPerez = new Atm(2,"Perez");
     $atmPrado = new Atm(3,"Prado");
     $atms = array($atmCamacho->id=>$atmCamacho, $atmPerez->id=>$atmPerez,$atmPrado->id=>$atmPrado);
     //print_r($atms); die();
     //print_r(http_response_code());
-    //print_r($_GET);die();
+    //print_r($_GET);
+    //print_r($_POST);die();
 
     switch($_SERVER["REQUEST_METHOD"]){
         case 'GET':
@@ -40,10 +41,40 @@
             break;
         case 'POST':
             //Variables o parametros
-            echo vectorAJson($_POST);
+            $id=count($atms)+1;
+            $descripcion=$_POST['descripcion'];
+            $atmNuevo = new Atm($id,$descripcion);
+            if($atms[$id]=$atmNuevo){
+                $respuesta = array("id"=>$id);
+            }else{
+                $respuesta = array("mensaje"=>"Error al dar de Alta Cajero");
+            }
+            echo vectorAJson($respuesta);
             break;
         case 'PUT':
-            echo "Solicitado por PUT";
+            if(isset($_GET['id'])){
+                $cajero=$_GET['id'];
+                //verificamos que exista el cajero solicitado
+                if(isset($atms[$cajero])){
+                    //print_r($_POST);die();
+                    if(isset($_POST['descripcion'])){
+                        $atms[$cajero]->descripcion=$_POST['descripcion'];
+                    }
+                    if(isset($_POST['cortes'])){
+                        $atms[$cajero]->cortes=$_POST['cortes'];
+                    }
+                    if(isset($_POST['cantidades'])){
+                        $atms[$cajero]->cantidades=$_POST['cantidades'];
+                    }
+                    $respuesta = $atms[$cajero]->imprimir();
+                }else{
+                    http_response_code(204);
+                    $respuesta = array("mensaje"=>"Cajero No existe");
+                }
+            }else{
+                $respuesta = array("mensaje"=>"Cajero No existe");
+            }
+            echo vectorAJson($respuesta);
             break;
         case 'DELETE':
             echo "Solicitado por DELETE";
