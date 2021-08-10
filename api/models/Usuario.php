69f9e0dd6c2197f6cf5bc127bebfe2f6e51803c8
Israel <israelhuallpara@gmail.com>
2021-08-10 19:55:46 -0400

    Método para actualizar un usuario

--- api/models/Usuario.php
+++ api/models/Usuario.php
@@ -19,6 +19,9 @@
                 "ci varchar(25),".
                 "apellidos varchar(100),".
                 "celular int(10),".
+                "eliminado bool,".
+                "fechaCreacion datetime,".
+                "fechaActualizacion datetime,".
                 "CONSTRAINT usuario_pk PRIMARY KEY (id_usuario), CONSTRAINT correo_uk UNIQUE (correo)".
             ")";
             $resultado = $cnx->query($consulta);
@@ -36,9 +39,21 @@
         function cambiarCorreo($correo){
             $this->correo = $correo;
         }
+        function obtenerNombres(){
+            return $this->nombres;
+        }
+        function cambiarNombres($nombres){
+            $this->nombres = $nombres;
+        }
+        function obtenerAplicacion(){
+            return $this->apliacion;
+        }
+        function cambiarAplicacion($aplicacion){
+            $this->aplicacion = $aplicacion;
+        }
         function iniciarSesion(){
             //Insertar el registro
-            $consultaInsert ='insert into usuarioI (correo, nombres, aplicacion) values (:correo, :nombres, :aplicacion);';
+            $consultaInsert ='insert into usuarioI (correo, nombres, aplicacion, eliminado, fechaCreacion, fechaActualizacion) values (:correo, :nombres, :aplicacion, false, now(), now());';
             $cnx = conexion();
             $consulta = $cnx->prepare($consultaInsert);
             $consulta->bindParam(':correo', $this->correo, PDO::PARAM_STR);
@@ -51,9 +66,26 @@
             $consulta->bindParam(':correo', $this->correo, PDO::PARAM_STR);
             $consulta->execute();
             $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
-            var_dump($resultados[0]);
+            $this->id_usuario = $resultados[0]["id_usuario"];            
+        }
+        function actualizar(){
+            $consultaActualizar = 'UPDATE usuarioI set correo=:correo, nombres=:nombres, aplicacion=:aplicacion, ci=:ci, apellidos =:apellidos, celular=:celular, fechaActualizacion = now() WHERE id_usuario = :id_usuario;';
+            $cnx = conexion();
+            $consulta = $cnx->prepare($consultaActualizar);
+            $consulta->bindParam(':correo', $this->correo, PDO::PARAM_STR);
+            $consulta->bindParam(':nombres', $this->nombres, PDO::PARAM_STR);
+            $consulta->bindParam(':aplicacion', $this->aplicacion, PDO::PARAM_STR);
+            $consulta->bindParam(':ci', $this->ci, PDO::PARAM_STR);
+            $consulta->bindParam(':apellidos', $this->apellidos, PDO::PARAM_STR);
+            $consulta->bindParam(':celular', $this->celular, PDO::PARAM_INT);
+            $consulta->bindParam(':id_usuario', $this->id_usuario, PDO::PARAM_INT);
+            $resultado = $consulta->execute();
+            return $resultado;
         }
     }
+    
     $u1 = new Usuario();
     $u1->cambiarCorreo("ijhm@gmail.com");
     $u1->iniciarSesion();
+    $u1->cambiarAplicacion("Google");
+    echo $u1->actualizar();
