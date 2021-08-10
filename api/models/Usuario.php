0704cde2fe62a278291cb0a6325dabec9e32c306
Israel <israelhuallpara@gmail.com>
2021-08-09 21:14:57 -0400

    Insertando usuario

--- api/models/Usuario.php
+++ api/models/Usuario.php
@@ -1,13 +1,13 @@
 <?php
     require_once('./../utils/conexion.php');
     class Usuario {
-        /*$id_usuario;
-        $correo;
-        $nombres;
-        $aplicacion;
-        $ci;
-        $apellidos;
-        $celular;*/
+        private $id_usuario="";
+        private $correo="";
+        private $nombres="";
+        private $aplicacion="";
+        private $ci="";
+        private $apellidos="";
+        private $celular=0;
 
         function Usuario(){
             $cnx = conexion();
@@ -19,10 +19,28 @@
                 "ci varchar(25),".
                 "apellidos varchar(100),".
                 "celular int(10),".
-                "CONSTRAINT usuario_pk PRIMARY KEY (id_usuario)".
+                "CONSTRAINT usuario_pk PRIMARY KEY (id_usuario), correo_uk UNIQUE (correo)".
             ")";
             $resultado = $cnx->query($consulta);
             if(!$resultado) return null;
         }
+        function iniciarSesion(){
+            //Insertar el registro
+            $consultaInsert ='insert into usuarioI (correo, nombres, aplicacion) values (:correo, :nombres, :aplicacion);';
+            $cnx = conexion();
+            $consulta = $cnx->prepare($consultaInsert);
+            $consulta->bindParam(':correo', $this->correo, PDO::PARAM_STR);
+            $consulta->bindParam(':nombres', $this->nombres, PDO::PARAM_STR);
+            $consulta->bindParam(':aplicacion', $this->aplicacion, PDO::PARAM_STR);
+            $consulta->execute();
+            //Seleccionar el registro
+            $consultaLee = 'SELECT * FROM usuarioI WHERE correo = :correo LIMIT 1 ;';
+            $consulta = $cnx->prepare($consultaLee);
+            $consulta->bindParam(':correo', $this->correo, PDO::PARAM_STR);
+            $consulta->execute();
+            $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+            var_dump($resultados[0]);
+        }
     }
     $u1 = new Usuario();
+    $u1->iniciarSesion();
