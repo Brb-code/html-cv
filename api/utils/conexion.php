a9b6560924a8dd0b02c0db391a3f4c037f063533
Israel <israelhuallpara@gmail.com>
2021-08-09 20:27:26 -0400

    Creando la tabla usuario

--- api/utils/conexion.php
+++ api/utils/conexion.php
@@ -1,21 +1,16 @@
 <?php
-    $host="sql176.main-hosting.eu";
-    $bd="u868164586_pruebaUBI";
-    $usuario="u868164586_ubi";
-    $contraseña="XlIT0=oy1$N";
-
-    function conexion($host, $bd, $usuario, $contraseña){
+    function conexion(){
+        $host="sql176.main-hosting.eu";
+        $bd="u868164586_pruebaUBI";
+        $usuario="u868164586_ubi";
+        $contrasenia='XlIT0=oy1$N';
         $cadenaConexion = "mysql:host=$host;dbname=$bd";
         try {
-            $cnx = new PDO($cadenaConexion, $usuario, $contraseña);
-            if($cnx) {
-                echo "Conectado!!!!";
-            } else {
-                echo "Error al conectarse";
-            }
+            $cnx = new PDO($cadenaConexion, $usuario, $contrasenia);
+            if($cnx) return $cnx;
+            else return null;
         } catch (PDOException $e) {
             var_dump($e);
+            return null;
         }
     }
-    
-    conexion($host, $bd, $usuario, $contraseña);
